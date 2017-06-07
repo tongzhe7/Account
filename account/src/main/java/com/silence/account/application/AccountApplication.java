1ac4ad741d233dc4afddeb83251a963ca0dc0628
ichuna <ichuna@163.com>
2017-06-07 11:09:42 +0800

    修改成我在bmob.cn站点的注册应用ID，可以实现了登录功能。 可以注册并使用。

--- account/src/main/java/com/silence/account/application/AccountApplication.java
+++ account/src/main/java/com/silence/account/application/AccountApplication.java
@@ -25,7 +25,8 @@ public class AccountApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Bmob.initialize(getApplicationContext(), "7930be502d4400a41d4f656fbd9c65cf");
+        //Bmob.initialize(getApplicationContext(), "7930be502d4400a41d4f656fbd9c65cf");
+        Bmob.initialize(getApplicationContext(), "15e0b29e8e1c7ec4200aa9a59b725645");
         sAccountApplication = this;
 //        refWatcher = LeakCanary.install(this);
 
